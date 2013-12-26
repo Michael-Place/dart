e4b58941e9e60ac3b6a7eaa8f9146f9aef5d0570
Michael Place <mplace@solstice-consulting.com>
2013-12-26 13:12:24 -0500

    Restructured project on file system. Groups now correspond exactly to file system folder structure. Deleted unnecessary assets and files.

---
+++ DartScoreBoard/Utilities/DSAppSkinner.h
@@ -0,0 +1,13 @@
+//
+//  DSAppSkinner.h
+//  DartScoreBoard
+//
+//  Created by Michael Place on 12/26/13.
+//  Copyright (c) 2013 Zheike. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSAppSkinner : NSObject
+
+@end
